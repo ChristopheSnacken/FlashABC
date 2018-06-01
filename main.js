6f5191863e57ed4aac94f8edae80d2fbb9bbaff4
Arjen <alphabravoab@gmail.com>
2018-06-01 12:02:33 +0200

    console

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ const questionTemplate ={
   falseAnswer2: "don't pick me either"
 }
 
-const h1q = object.create(questionTemplate)
+const h1q = Object.create(questionTemplate)
 
 h1q.question="What does the H1 tag do?"
 h1q.trueanswer="It formats the style inbetween the tags as header 1 which is changeable in the CSS"
 h1q.falseAnswer1="magic"
 h1q.falseAnswer2="Drives my car"
 
-const divq = object.create(questionTemplate)
+const divq = Object.create(questionTemplate)
 
 divq.question="What does the div tag do?"
 divq.trueanswer="It creates an empty container which I can manipulate in CSS"
 divq.falseAnswer1="makes my lunch"
 divq.falseAnswer2="Runs the world"
+
+
+console.log(h1q)
+
+console.log(questionTemplate.children)
